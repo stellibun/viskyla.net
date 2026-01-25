a32d27cd0e96f5283867355a7577ebe9a8722dd5
viskyla <novapeache@gmail.com>
2026-01-25 17:54:32 -0500

    annc

--- assets/scripts/load.js
+++ assets/scripts/load.js
@@ -9,5 +9,6 @@ window.onload=()=>{
         document.getElementById("nav").innerHTML = data;
     });
 
-    document.getElementById("container").insertAdjacentHTML("beforebegin","<p style=\"height: auto;background: var(--accent-2);color: var(--text-colour);text-align: center;text-anchor: middle;padding: 2px;margin-bottom: 10px;\"><b>Announcement message across entire website</b></p>");
-}
+    msg = "I will be remaking the entire website, this old one will probably be under some random directory, but as of right now, this is gonna all change soon. The new website will be <a href=\"/new\">under /new</a>";
+    document.getElementById("container").insertAdjacentHTML("beforebegin","<p style=\"height: auto;background: var(--accent-2);color: var(--text-colour);text-align: center;text-anchor: middle;padding: 2px;margin-bottom: 10px;\"><b>"+msg+"</b></p>");
+};
