065c01cba0dbfc97be3d5f15c1f076592ab0c879
viskyla <novapeache@gmail.com>
2026-01-25 23:29:47 -0500

    fixy wixy

--- assets/scripts/load.js
+++ assets/scripts/load.js
@@ -10,5 +10,5 @@ window.onload=()=>{
     });
 
     msg = "I will be remaking the entire website, this old one will probably be under some random directory, but as of right now, this is gonna all change soon. The new website will be <a href=\"/new/\">under /new/</a>";
-    document.getElementById("container").insertAdjacentHTML("beforebegin","<p style=\"height: auto;background: var(--accent-2);color: var(--text-colour);text-align: center;text-anchor: middle;padding: 2px;margin-bottom: 10px;padding-right: 10%;padding-left: 10%;\"><b>"+msg+"</b></p>");
+    document.getElementById("container").insertAdjacentHTML("beforebegin","<div style=\"height: auto;background: var(--accent-2);color: var(--text-colour);text-align: center;text-anchor: middle;padding: 2px;margin-bottom: 10px;\"><p style=\"margin: auto;max-width: 850px;margin-top: 5px;margin-bottom: 5px;\"><b>"+msg+"</b></p></div>");
 };
