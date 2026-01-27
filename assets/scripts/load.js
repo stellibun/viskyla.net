f941cadcf3cc681efaf1577418b6a08cc2a9f794
viskyla <novapeache@gmail.com>
2026-01-26 19:58:55 -0500

    upd2

--- assets/scripts/load.js
+++ assets/scripts/load.js
@@ -9,6 +9,6 @@ window.onload=()=>{
         document.getElementById("nav").innerHTML = data;
     });
 
-    msg = "I will be remaking the entire website, this old one will probably be under some random directory, but as of right now, this is gonna all change soon. The new website will be under <a href=\"https://dev.viskyla.net\">dev.viskyla.net</a>";
+    msg = "I will be remaking the entire website, this old one will probably be under some random directory, but as of right now, this is gonna all change soon. The new website will be under <a href=\"https://stellibun.net\">stellibun.net</a>";
     document.getElementById("container").insertAdjacentHTML("beforebegin","<div style=\"height: auto;background: var(--accent-2);color: var(--text-colour);text-align: center;text-anchor: middle;padding: 2px;margin-bottom: 10px;\"><p style=\"margin: auto;max-width: 850px;margin-top: 5px;margin-bottom: 5px;\"><b>"+msg+"</b></p></div>");
 };
